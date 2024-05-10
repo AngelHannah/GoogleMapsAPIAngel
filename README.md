d70cf8ce01137855ce93719ca85a07a84f29f51e
Hannah <87335534+AngelHannah@users.noreply.github.com>
2024-05-09 21:43:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoogleMapsAPIAngel
+ Google Maps practice where we call the Maps API and display the map.
---
+++ app/src/main/java/com/example/googlemapsapi_angel/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.googlemapsapi_angel;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //Reference for the map
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+
+        //Grab the map fragment and then call getMapAsync()
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
